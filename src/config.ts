50d4000b44888da50b855b81b2e37077e5940395
archimonde12 <39382448+archimonde12@users.noreply.github.com>
2020-12-02 08:50:46 +0700

    fixed some config

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,10 @@ export const trxusdtRateURL =
   process.env.TRXUSDTRATEURL ||
   "https://api.coingecko.com/api/v3/coins/tron/tickers?exchange_ids=binance&page=1";
 
-export const redisUri = process.env.REDIS;
+export const redisUri =
+  process.env.REDIS || "redis-15381.c245.us-east-1-3.ec2.cloud.redislabs.com";
 export const redisPort = 15381;
-export const redisAuth = `0xCDbcCuRusFwrpSVOKzQTV2bVgOax1J`;
+export const redisAuth = process.env.AUTH || `0xCDbcCuRusFwrpSVOKzQTV2bVgOax1J`;
 export const target_currency = "USDT";
 export const key = "appname.trx.usdt.rate";
 export const saveDataPeriod = 10; //seconds
