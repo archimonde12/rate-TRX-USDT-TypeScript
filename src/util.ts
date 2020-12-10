9cc6e0d082070f2c22ca58d186fef18f149055d2
archimonde12 <39382448+archimonde12@users.noreply.github.com>
2020-12-10 14:26:48 +0700

    solve rate limit by using local

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 export const checkTimeFunc = (timeCheck: number, checkTime: number) => {
+  //Check dữ liệu có đảm bảo cập nhật mới nhất hay không
   const now = new Date().getTime();
   return now - timeCheck < checkTime * 1000 * 60;
 };
